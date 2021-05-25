f26d87d196d1a3cfccf103584d4a3cc406c31dbc
Jray <1075860716@qq.com>
2021-05-25 17:14:42 +0800

    ✨ feat: add red black tree

---
+++ RBTree.h
@@ -0,0 +1,55 @@
+#include "DNSPacket.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+enum type
+{
+    RED,
+    BLACK
+};
+
+typedef struct MyData
+{
+    uint32_t time;
+    DNSRecord record;
+} MyData;
+
+typedef struct Key
+{
+    DNSQType qtype;
+    char name[255];
+} Key;
+
+struct Node
+{
+    Key key;
+    struct MyData myData;
+    struct Node *left;
+    struct Node *right;
+    struct Node *parent;
+    enum type color;
+};
+
+struct Queue
+{
+    struct Node *data;
+    struct Queue *next;
+};
+
+struct Queue *front = NULL;
+struct Queue *rear = NULL;
+
+struct Node *pfront()
+{
+    struct Node *data;
+    data = front->data;
+    return data;
+}
+
+struct Node *initRBTree();
+struct Node *RB_insert(struct Node *T, Key key, MyData mydata);
+struct Node *BST_search(struct Node *root, Key key);
+struct Node *RB_delete(struct Node *T, struct Node *z);
+void deleteAll(struct Node *root);
