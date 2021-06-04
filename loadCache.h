ae48c0c777ec1848fe0652d7cc978e6f0325e276
Jray <1075860716@qq.com>
2021-06-04 16:17:05 +0800

    🐳 chore: Move yyjson to lib

--- loadCache.h
+++ loadCache.h
@@ -1,7 +1,7 @@
 #ifndef LOADCACHE_H
 #define LOADCACHE_H
 #include "cli.h"
-#include "yyjson.h"
+#include "lib/yyjson.h"
 
 void writeCache(char *fname, DNSRD_RUNTIME *runtime);
 void loadCache(char *fname, DNSRD_RUNTIME *runtime);
