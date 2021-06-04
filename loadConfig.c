cb53b04cde15266cf90ad56fbdd400f6ce2ce217
Jray <1075860716@qq.com>
2021-06-04 19:58:32 +0800

    ✨ feat: support CNAME for hostfile

--- loadConfig.c
+++ loadConfig.c
@@ -1,4 +1,5 @@
 #include "loadConfig.h"
+#include "DNSPacket.h"
 #include <ws2tcpip.h>
 
 void loadConfig(char *fname, LRUCache *lruCache) {
@@ -14,6 +15,7 @@ void loadConfig(char *fname, LRUCache *lruCache) {
             int flag = 0;
             int i = 0, j = 0;
             int flag_v6 = 0;
+            int flag_cname = 0;
             for (i = 0; i < strlen(buf); i++) {
                 if (buf[i] != ' ' && buf[i] != '\t' && buf[i] != '#' && buf[i] != '\n') {
                     bufs[flag][i - j] = buf[i];
@@ -28,11 +30,16 @@ void loadConfig(char *fname, LRUCache *lruCache) {
                 if (buf[i] == ':') {
                     flag_v6 = 1;
                 }
+                if (!flag && ((buf[i] >= 'a' && buf[i] <= 'z') || (buf[i] >= 'A' && buf[i] <= 'Z'))) {
+                    flag_cname = 1;
+                }
             }
             bufs[flag][i - j] = 0;
             Key key;
             if (flag_v6) {
                 key.qtype = AAAA;
+            } else if (!flag_v6 && flag_cname) {
+                key.qtype = CNAME;
             } else {
                 key.qtype = A;
             }
@@ -59,6 +66,13 @@ void loadConfig(char *fname, LRUCache *lruCache) {
                     memcpy(record.rdata, ipv6data.u.Byte, sizeof(uint8_t) * 16);
                     record.rdataLength = sizeof(uint8_t) * 16;
                 }
+            } else if (!flag_v6 && flag_cname) {
+                record.rdata = (char *)malloc(257);
+                char tmp[257];
+                strcpy_s(tmp, 257, buf1);
+                strcpy_s(record.rdataName, 257, buf1);
+                toQname(tmp, record.rdata);
+                record.rdataLength = strnlen_s(buf1, 256) + 2;
             } else {
                 record.rdata = (char *)malloc(sizeof(uint8_t) * 4);
                 struct in_addr ipv4data;
